7fa6316669e6ac8ba4e71b8c716c0ac4b81a0a9f
ogaksk <ogawa@ogawa-air.local>
2014-07-01 21:29:10 +0900

    ファイル転送用準備

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
+  , fs = require('fs');
 
 var app = express();
 
@@ -48,47 +49,51 @@ io.sockets.on("connection", function(socket) {
     message : "…", // フキダシの中身
   };
 
-console.log("connect new client.");
+  console.log("connect new client.");
 
-// ログイン名を送ってきた時は新規ログインの処理
-socket.on("name", function(text) {
-  console.log("name: " + text);
-  player.login_name = text;
-  socket.broadcast.emit("name", player.login_name);
-  // それまでにログインしてるプレイヤー情報を送る
-  for (var i in player_list) {
-    var c = player_list[i]; // ログイン中プレイヤーリストからプレイヤー情報取得
-    socket.emit("name", c.login_name);
-    socket.emit("position:" + c.login_name,
-                c.x + "," + c.y + "," + c.direction);
-    socket.emit("message:" + c.login_name, c.message);
-  }
-  // ログイン中プレイヤーリストへの登録
-  player_list[ player.login_name ] = player;
-});
+  // ログイン名を送ってきた時は新規ログインの処理
+  socket.on("name", function(text) {
+    console.log("name: " + text);
+    player.login_name = text;
+    socket.broadcast.emit("name", player.login_name);
+    // それまでにログインしてるプレイヤー情報を送る
+    for (var i in player_list) {
+      var c = player_list[i]; // ログイン中プレイヤーリストからプレイヤー情報取得
+      socket.emit("name", c.login_name);
+      socket.emit("position:" + c.login_name,
+                  c.x + "," + c.y + "," + c.direction);
+      socket.emit("message:" + c.login_name, c.message);
+    }
+    // ログイン中プレイヤーリストへの登録
+    player_list[ player.login_name ] = player;
+  });
 
-// 移動処理
-socket.on("position", function(pos) {
-  // console.log("position:" + player.login_name + "x="+ pos.x + "y=" + pos.y);
-  player.x = pos.x;
-  player.y = pos.y;
-  player.rotation = pos.rotation;
-  socket.broadcast.emit("position:" + player.login_name, pos);
-});
+  // 移動処理
+  socket.on("position", function(pos) {
+    // console.log("position:" + player.login_name + "x="+ pos.x + "y=" + pos.y);
+    player.x = pos.x;
+    player.y = pos.y;
+    player.rotation = pos.rotation;
+    socket.broadcast.emit("position:" + player.login_name, pos);
+  });
 
-// // こちらがメッセージを受けた時の処理
-// socket.on("message", function(text) {
-//   console.log("message:" + player.login_name + " " + text);
-//   player.message = text;
-//   socket.broadcast.emit("message:" + player.login_name, text);
-// });
+  // // こちらがメッセージを受けた時の処理
+  // socket.on("message", function(text) {
+  //   console.log("message:" + player.login_name + " " + text);
+  //   player.message = text;
+  //   socket.broadcast.emit("message:" + player.login_name, text);
+  // });
 
-// 切断した時の処理
-socket.on("disconnect", function() {
-  console.log("disconnect:" + player.login_name);
-  socket.broadcast.emit("disconnect:" + player.login_name);
-  // ログイン中プレイヤーリストからの削除
-  delete player_list[ player.login_name ];
-});
+  socket.on("itemget" ,function() {
+    var url = 'contents/a.zip';
+  })
+
+  // 切断した時の処理
+  socket.on("disconnect", function() {
+    console.log("disconnect:" + player.login_name);
+    socket.broadcast.emit("disconnect:" + player.login_name);
+    // ログイン中プレイヤーリストからの削除
+    delete player_list[ player.login_name ];
+  });
 
 });
