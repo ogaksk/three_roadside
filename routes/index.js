6056a92bef7f4bc5620c863ab1357b6a1dfcfa82
ogaksk <ogawa@ogawa-air.local>
2016-04-06 00:45:17 +0900

    ua出し分けできてきた

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,16 @@
  */
 
 exports.index = function(req, res){
+  // ユーザーエージェント処理
+  var incompatibles = ["iphone", "ipad", "android"];
+  var ua = req.headers['user-agent'].toLowerCase();
+
+  for (var i = 0; i < incompatibles.length; i ++) {
+    if(ua.indexOf(incompatibles[i]) != -1){
+      res.render('incompatible', { title: 'sorry!!' });
+      return
+    }
+  }
+
   res.render('index', { title: 'ロードサイド3D' });
 };
