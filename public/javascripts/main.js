40c0a494340cb4ef0ef669d1a5b62efb0adf7552
ogaksk <ogawa@ogawa-air.local>
2016-09-23 16:21:45 +0900

    オブジェクトは配置できた

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -453,6 +453,7 @@
       lenderNPC();
       toLenderingStart(renderer);
       isAdmin(name);
+      createTweetObject();
     });
 
     // オリジナル作成
@@ -512,6 +513,49 @@
       scene.add(itemObject);
     });
 
+    // ツイートオブジェクトまわり
+    function createTweetObject () {
+      var tweetObject;
+      var tweetModelMaterial;
+
+      async.waterfall([
+        function (callback) {
+          jsonLoader.load("./javascripts/json_objects/mp3.js", function(geometry, materials) { 
+            tweetModelMaterial = new THREE.MeshFaceMaterial( materials );
+            tweetObject = new THREE.Mesh( geometry, tweetModelMaterial );
+            for (var l = 0; l < tweetObject.material.materials.length; l++) {
+              tweetObject.material.materials[l].ambient = tweetObject.material.materials[l].color;
+            }
+            callback();
+          });
+          
+        }, function (callback) {
+          async.forEachSeries(Object.keys(TweetMaps), function(tweetPoint, callback_each) {
+            for (var i in TweetMaps[tweetPoint].locations) {
+              var tweet = tweetObject.clone();
+              tweet.material = tweetModelMaterial.clone();
+              tweet.position.set(TweetMaps[tweetPoint].locations[i][0] * BLOCK_SIZE, 10, TweetMaps[tweetPoint].locations[i][1] * BLOCK_SIZE); 
+              tweet.scale.set( TweetMaps[tweetPoint].scale , TweetMaps[tweetPoint].scale , TweetMaps[tweetPoint].scale );
+
+              tweet.rotation.set(0, -((TweetMaps[tweetPoint].rotation - 90) * Math.PI / 180), 0)
+              
+              // for (var l = 0; l < tweet.material.materials.length; l++) {
+              //   tweet.material.materials[l].ambient = tweet.material.materials[l].color;
+              // }
+              scene.add(tweet);
+            }
+            callback_each();
+          }, function(err) {
+          });
+        callback();
+        }
+      ], 
+      function(err) { 
+        if (err) {
+          throw err;
+        }
+      });
+    }
 
     // 床
     var pGeometry = new THREE.PlaneGeometry(MAP_BLOCK_SIZE*10000, MAP_BLOCK_SIZE*10000);
---
+++ public/javascripts/tweetmaps.js
@@ -0,0 +1,168 @@
+var TweetMaps = {
+  aeon: { 
+    description: "イオンにいます", 
+    scale: 30,
+    rotation: 0,
+    locations: [
+      [10, 48]
+    ]
+  },
+
+  suzuki: {
+    description: "鈴木にいます",
+    scale: 30,
+    rotation: 0,
+    locations: [
+      [100, 40], [100, 60], 
+    ]
+  },
+
+  alpen: { 
+    description: "アルペンにいます", 
+    scale: 30,
+    rotation: 90,
+    locations: [
+      [80, 20], [80, 80]
+    ]
+  }, 
+  videoa: { 
+    description: "VIAにいます", 
+    scale: 30,
+    rotation: 90,
+    locations: [
+      [20, 30], [20, 70]
+
+    ]
+  }, 
+
+  haruyama_l: { 
+    description: "はるやまにいます", 
+    scale: 30,
+    rotation: 180,
+    locations: [
+      [63, 20]
+    ]
+  },
+
+  haruyama_r: { 
+    description: "はるやまにいます", 
+    scale: 30,
+    rotation: 0,
+    locations: [
+      [57, 80]
+    ]
+  },
+
+  kutsuryu: {
+    description: "靴流通にいます", 
+    scale: 30,
+    rotation: 90,
+    locations: [
+      [40, 20], [40, 80]
+    ]
+  },
+
+  // atom: { 
+  //   description: "./javascripts/json_objects/atom1006.js", 
+  //   scale: 5,
+  //   rotation: 0,
+  //   locations: [
+  //     [30, 40], [40, 40], [50, 40], [60, 40], [70, 40], [80, 40], 
+  //     [35, 60], [45, 60], [55, 60], [65, 60], [75, 60]
+  //   ]
+  // },
+
+
+  // tuxulogo: {
+  //   description: "./javascripts/json_objects/tuxulogo.js",
+  //   scale: 50, 
+  //   rotation: 90,
+  //   locations: [
+  //     [30, 30], [40, 30], [50, 30], [60, 30], [70, 30], [80, 30],
+  //     [30, 70], [40, 70], [50, 70], [60, 70], [70, 70], [80, 70]
+  //   ]
+  // },
+
+  cosmo: {
+      description: "コスモにいます",
+      scale: 30,
+      rotation: 90,
+      locations: [
+        [50, 20], [50, 80]
+      ]
+  },
+
+  // cosmo_kanban: {
+  //     description: "./javascripts/json_objects/cosmo_kanban.js",
+  //     scale: 62,
+  //     rotation: 90,
+  //     locations: [
+  //       [52, 22], [52, 82]
+  //     ]
+  // },
+
+  korochan_l: { 
+    description: "コロちゃんにいます", 
+    scale: 30,
+    rotation: 180,
+    locations: [
+      [30, 20]
+    ]
+  },
+
+    korochan_r: { 
+    description: "コロちゃんにいます", 
+    scale: 30,
+    rotation: 0,
+    locations: [
+      [30, 80]
+    ]
+  },
+
+  ramenshop_l: { 
+    description: "ラーメンショップにいます", 
+    scale: 30,
+    rotation: 270,
+    locations: [
+      [70, 20]
+    ]
+  },
+
+  ramenshop_r: { 
+    description: "ラーメンショップにいます", 
+    scale: 30,
+    rotation: 90,
+    locations: [
+      [70, 80]
+    ]
+  },
+
+  yellowhat: { 
+    description: "イエローハットにいます", 
+    scale: 30,
+    rotation: 90,
+    locations: [
+      [20, 20], [20, 80]
+    ]
+  },
+
+  // tukushi: {
+  //   description: "./javascripts/json_objects/tukushi.js", 
+  //   scale: 100,
+  //   rotation: 90,
+  //   locations: [
+  //     [20, 10], [30, 10], [40, 10], [50, 10], [60, 10], [70, 10], [80, 10], 
+  //     [20, 90], [30, 90], [40, 90], [50, 90], [60, 90], [70, 90], [80, 90]
+  //   ]
+  // },
+
+  // video: {
+  //   description: "./javascripts/json_objects/video.js",
+  //   scale: 6,
+  //   rotation: 0,
+  //   locations: [
+  //     [35, 40], [45, 40], [55, 40], [65, 40], [75, 40],
+  //     [30, 60], [40, 60], [50, 60], [60, 60], [70, 60], [80, 60],
+  //   ]
+  // }
+};
